1866e9e3e945478c89a864f77aeb00b3ba6b0f9f
QUERY-GANI <ahmadxgani01@gmail.com>
2021-06-09 09:53:05 +0700

    error handilng, Ref: https://www.dicoding.com/academies/256/tutorials/13998

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-import { isCoffeeMachineReady } from "./state.js";
-
-console.log(isCoffeeMachineReady);
-
-console.log("Menyalakan mesin kopi");
-console.log("Menggiling biji kopi");
-console.log("Memanaskan air");
-console.log("Mencampurkan air dan kopi");
-console.log("Menuangkan kopi ke dalam gelas");
-console.log("Menuangkan susu ke dalam gelas");
-console.log("Kopi Anda sudah siap!");
+// const json = '{ "name": "Yoda", "age": 20 }'; // no error
+// const json = 'bad json'; // error
+const json = '{ "age": 15, "name": "shinigami" }'; // does not raise an error but will cause a bug
+     
+try {
+    const user = JSON.parse(json);
+    if (!user.name) {
+        throw new SyntaxError("'name' is required.");
+    }
+    undefinedVariabel;
+    console.log(user.name);
+    console.log(user.age);
+} catch (error) {
+    if (error instanceof SyntaxError) {
+        console.log(`JSON Error: ${error.message}`);
+    } else if (error instanceof ReferenceError) {
+        console.log(error.message);
+    } else {
+        console.log(error.stack);
+    }
+}
