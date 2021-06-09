c36c9c8344bcb2c168402a55052f12372aef4a2f
QUERY-GANI <ahmadxgani01@gmail.com>
2021-06-09 10:38:56 +0700

    custom error msg, Ref: https://www.dicoding.com/academies/256/tutorials/14002?from=13998

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
+class CustomError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "CustomError";
+    }
+}
+
 // const json = '{ "name": "Yoda", "age": 20 }'; // no error
 // const json = 'bad json'; // error
-const json = '{ "age": 15, "name": "shinigami" }'; // does not raise an error but will cause a bug
+const json = '{ "age": 15 }'; // does not raise an error but will cause a bug
      
 try {
-    const user = JSON.parse(json);
+    let user = JSON.parse(json);
+ 
     if (!user.name) {
-        throw new SyntaxError("'name' is required.");
+        throw new CustomError("'name' is required.");
     }
-    undefinedVariabel;
+    if (!user.age) {
+        throw new CustomError("'age' is required.");
+    }
+ 
     console.log(user.name);
     console.log(user.age);
 } catch (error) {
     if (error instanceof SyntaxError) {
-        console.log(`JSON Error: ${error.message}`);
+        console.log(`JSON Syntax Error: ${error.message}`);
+    } else if (error instanceof CustomError) {
+        console.log(`Invalid data: ${error.message}`);
     } else if (error instanceof ReferenceError) {
         console.log(error.message);
     } else {
